a39650977d816494b6ae33a9d1b68fae2d32501a
murilotet <58227071+murilotet@users.noreply.github.com>
2024-03-26 19:40:15 -0300

    Add files via upload

---
+++ Decoder/PartitionDecoder.h
@@ -0,0 +1,27 @@
+#include "MultiscaleTransform.h"
+#include "Hierarchical4DDecoder.h"
+#include <math.h>
+#include <string.h>
+
+#ifndef TRANSFORMPARTITION_H
+#define TRANSFORMPARTITION_H
+
+#define NOSPLITFLAGSYMBOL 0
+#define INTRAVIEWSPLITFLAGSYMBOL 1
+#define INTERVIEWSPLITFLAGSYMBOL 2
+#define MINIMUM_BITPLANE_PRECISION 5
+
+
+class PartitionDecoder {
+public:  
+    int mPartitionCodeMaxLength;        /*!< Maximum length of the partition tree code string */
+    int mEvaluateOptimumBitPlane;       /*!< Toggles the optimum bit plane evaluation procedure on and off */
+    int mUseSameBitPlane;               /*!< Forces to use the same minimum bitplane for all subblocks */
+    Block4D mPartitionData;             /*!< DCT of all subblocks of the partition */
+    PartitionDecoder(void);
+    void DecodePartition(Hierarchical4DDecoder &entropyDecoder, MultiscaleTransform &mt);
+    void DecodePartitionStep(int *position, int *length, Hierarchical4DDecoder &entropyDecoder, MultiscaleTransform &mt);
+};
+
+#endif /* TRANSFORMOPTIMIZATION_H */
+
